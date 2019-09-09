ed724c42186055f025e6f9cfed3107e252a8b26f
Mathieu Zwygart <mathieu@zwygart.net>
2019-09-09 22:20:17 +0200

    WIP: timeline

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="timelineItem">
+        <h2 class="timelineItem__title">{{title}}</h2>
+        <time class="timelineItem__date">{{date}}</time>
+        <p><slot></slot></p>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        props: ['title', 'date']
+    }
+</script>
+
+<style lang="scss">
+    .timelineItem {
+        margin: 1rem 0 1rem 3rem;
+        padding: 2rem 3rem;
+        border: 1px solid #333;
+        position: relative;
+        overflow: visible;
+
+        &::before {
+            display: inline-block;
+            content: '●';
+            position: absolute;
+            top: 0; left: -10px;
+            transform: translateX(-50%);
+            color: $contrast;
+            font-size: 2rem;
+        }
+
+        @media (min-width: 768px){
+            width: calc(50% - 3rem);
+            margin: 1rem 3rem 1rem 0;
+
+            &:nth-child(even) {
+                margin: 1rem 0 1rem 3rem;
+                margin-left: auto;
+            }
+        }
+
+        &__wrapper {
+            position: relative;
+
+            &::before {
+                content: '';
+                display: block;
+                position: absolute;
+                width: 5px;
+                top: 1rem; bottom: 1rem; left: 0;
+                transform: translateX(-50%);
+                background: $contrast;
+            }
+
+            @media (min-width: 768px){
+                &::before {
+                    left: 50%;
+                }
+            }
+        }
+
+        &__title {
+            font-size: 1.8rem;
+            display: inline-block;
+            margin-right: 1rem;
+        }
+
+        &__date {
+            font-size: 1.6rem;
+            display: inline-block;
+
+            &::before {
+                display: inline-block;
+                content: '●';
+                font-size: .5rem;
+                vertical-align: middle;
+                margin-right: 1rem;
+            }
+        }
+    }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <Layout>
     <Banner/>
 
-    <section id="competences" class="container--overSized negativeTopMargin bg-white">
+    <section id="competences" class="container--overSized negativeTopMargin bg-white py-big">
       <div class="container">
         <div class="row">
           <div class="col-md-10 offset-md-1">
@@ -39,21 +39,43 @@
       </div>
     </section>
 
-    <section class="bg-black">
+    <section id="parcours" class="bg-black py-big">
       <div class="container">
         <div class="row">
-          <div class="col-10 offset-md-1">
+          <div class="col-12 col-md-10 offset-md-1">
             <h1>Mon parcours</h1>
-            <p>2018 Intérvant pour la formation "Création de thème WordPress" de Procom
-    Depuis 2016 chez 8bitstudio</p>
+
+            <div class="timelineItem__wrapper clearfix">
+              <timeline-item date="2018" title="SAWI">Intérvant pour la formation "Création de thème WordPress" de Procom</timeline-item>
+              <timeline-item date="2016" title="8bitstudio">Hello</timeline-item>
+              <timeline-item date="2014" title="Base7booking">Hello</timeline-item>
+              <timeline-item date="2012" title="Suisseo">Hello</timeline-item>
+              <timeline-item date="2009" title="Solugest">Hello</timeline-item>
+            </div>
           </div>
         </div>
       </div>
-      
-    
     </section>
 
-    
+    <section id="realisations" class="py-big">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-10 offset-md-1">
+            <h1>Mes réalisations</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="contact" class="py-big">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-10 offset-md-1">
+            <h1>Me contacter</h1>
+          </div>
+        </div>
+      </div>
+    </section>
     
     
     
@@ -67,13 +89,15 @@
 
 <script>
 import Banner from '~/components/Banner.vue'
+import TimelineItem from '~/components/TimelineItem.vue'
 
 export default {
   metaInfo: {
     title: 'Hello, world!'
   },
   components: {
-    Banner
+    Banner,
+    TimelineItem,
   }
 }
 </script>
